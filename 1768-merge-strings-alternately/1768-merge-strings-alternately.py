d968cbb3d9cb6ad2babf73dbfc502a47938d637c
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-09 13:58:34 +0900

    Time: 43 ms (13.42%), Space: 17.8 MB (12.74%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,15 +1,5 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        """將兩個字串交互穿插
-
-        Args:
-            str1: 第一個字串
-            str2: 第二個字串
-
-        Returns:
-            一個新的字串，其中兩個字串的字符交互穿插
-        """
-
         result = ""
         for i in range(min(len(word1), len(word2))):
             result += word1[i] + word2[i]
