4db415e1e3c42ea2375e512b7b5fbfaa186c38f1
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-26 12:30:40 +0900

    Time: 328 ms (86.24%), Space: 67.9 MB (7.56%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
     employees['bonus'] = employees[(employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M'))][['salary']]
-    return employees[['employee_id', 'bonus']].fillna(0)
+    return employees[['employee_id', 'bonus']].fillna(0).sort_values(by='employee_id')
